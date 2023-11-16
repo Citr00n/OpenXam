25d13bcee7553ac25d2f8e15c09503fca5ce3589
Restyled.io <commits@restyled.io>
2023-11-16 09:51:51 +0000

    Restyled by autopep8

--- OpenXam/settings.py
+++ OpenXam/settings.py
@@ -56,8 +56,7 @@ SESSION_COOKIE_SECURE = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
