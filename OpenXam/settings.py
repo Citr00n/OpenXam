723b27103dd385a8a0d07113799c1327dfbca041
Citr0n <citroncorp@mail.ru>
2023-11-16 13:15:07 +0300

    Edited settings.py

--- OpenXam/settings.py
+++ OpenXam/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8a1d8)^3c!&)sxb^1vi!uyo+@eh+wqfjsg@a$i4l2n@$5o#j(b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', '0.0.0.0']
 
 
 # Application definition
